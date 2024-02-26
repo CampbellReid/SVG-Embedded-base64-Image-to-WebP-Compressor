b3b245140a605f5f43ae90474aa59ba9bb7029fa
Campbell Reid <campbelljreid@gmail.com>
2024-02-26 19:26:11 +1000

    Version 3: Extract the image href and print them to the console

--- compress.py
+++ compress.py
@@ -1,13 +1,15 @@
 import xml.etree.ElementTree as ET
 
+# Version 1: Read input.svg and print it to the console
 # Version 2: Find all the image tag and print them to the console
+# Version 3: Extract the image href and print them to the console
 def main():
     tree = ET.parse('input.svg')
     root = tree.getroot()
     
     for elem in root.iter():
         if elem.tag.endswith('image'):
-            print(elem.tag, elem.attrib)
+            print(elem.attrib['href'])
 
 if __name__ == "__main__":
     main()
