0ada2645ea09a964283e1df6ccd75449d28cf2a7
Campbell Reid <campbelljreid@gmail.com>
2024-02-26 19:22:39 +1000

    Version 1: Parse the svg and print to the console

--- compress.py
+++ compress.py
@@ -1,5 +1,10 @@
+import xml.etree.ElementTree as ET
+
+# Version 1: Read input.svg and print it to the console
 def main():
-    print("Compressing files")
+    tree = ET.parse('input.svg')
+    root = tree.getroot()
+    print(ET.tostring(root, encoding='utf8').decode('utf8'))
 
 if __name__ == "__main__":
     main()
