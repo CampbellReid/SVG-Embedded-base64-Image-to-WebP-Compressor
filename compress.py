99595e27475bd8fa29c2128792df42fbc34d6580
Campbell Reid <campbelljreid@gmail.com>
2024-02-26 19:34:08 +1000

    Version 5: Convert the base64 data to binary and print the binary data to the console

--- compress.py
+++ compress.py
@@ -1,9 +1,11 @@
 import xml.etree.ElementTree as ET
+import base64
 
 # Version 1: Read input.svg and print it to the console
 # Version 2: Find all the image tag and print them to the console
 # Version 3: Extract the image href and print them to the console
 # Version 4: Extract the image href and print the data type and data to the console
+# Version 5: Convert the base64 data to binary and print the binary data to the console
 def main():
     tree = ET.parse('input.svg')
     root = tree.getroot()
@@ -12,10 +14,11 @@ def main():
         if elem.tag.endswith('image'):
             href_data = elem.attrib['href'].split(',')
             data_type = href_data[0].split(';')[0].split(':')[1].split('/')[1]
-            data_base64 = href_data[1]
+            data_base64_bytes = href_data[1].encode('ascii')
             
-            print(data_type)
-            print(data_base64)
+            data_bytes = base64.b64decode(data_base64_bytes)
+            
+            print(data_bytes)
 
 if __name__ == "__main__":
     main()
